9e805fe48704c18766775ecb372c48ffade16951
yongxinz <hi_nature@yeah.net>
2021-09-30 09:49:59 +0800

    snow_flake_id 通过配置文件配置

--- app/modules/home/homemodel/alloc_snow_flake_id.go
+++ app/modules/home/homemodel/alloc_snow_flake_id.go
@@ -1,9 +1,15 @@
 package homemodel
 
-import "gid/app/libs"
+import (
+	"gid/app/libs"
+
+	"github.com/hulklab/yago"
+)
 
 func (s *Service) NewAllocSnowFlakeId() (a *libs.Worker, err error) {
-	return libs.NewWorker(1)
+	conf := yago.Config.GetStringMap("app")
+	id := conf["snow_flake_id"].(int64)
+	return libs.NewWorker(id)
 }
 
 func (s *Service) SnowFlakeGetId() int64 {
